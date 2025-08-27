6e49996d295635bde9c9bd7916e7b3fc662a96e3
muhammed sinan <sinanzzsinu70@gmail.com>
2025-08-27 11:38:00 +0530

    Initial

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light, 
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(fontSize: 16.0, color: Colors.black87),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+  );
+}
