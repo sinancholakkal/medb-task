2d86daea138be655da89bc268a60e15910d2c718
muhammed sinan <sinanzzsinu70@gmail.com>
2025-08-28 12:18:07 +0530

    drawer personal item added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:medb_task/utils/app_color.dart';
 import 'package:medb_task/widgets/app_sizedbox.dart';
 import 'package:medb_task/widgets/image_button.dart';
+import 'package:medb_task/widgets/text_feild.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -20,15 +21,20 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       drawer: Drawer(
-        
+        backgroundColor: Colors.grey[100],
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            AppSizedBox.h60,
+            AppSizedBox.h70,
             SizedBox(
               width: 45,
               height: 45,
-              child: Image.asset("asset/medb-logo-png-BRGSYv_I.png",))
+              child: Image.asset("asset/medb-logo-png-BRGSYv_I.png",)),
+            AppSizedBox.h24,
+              ListTile(
+                leading: CircleAvatar(),
+                title: TextWidget(text: "Muhammed Sinan",color: kBlack,fontWeight: FontWeight.bold,size: 22,),
+              )
             
           ],
         ),
--- lib/widgets/app_sizedbox.dart
+++ lib/widgets/app_sizedbox.dart
@@ -5,7 +5,7 @@ class AppSizedBox {
   static const SizedBox h20 = SizedBox(height: 20);
   static const SizedBox h16 = SizedBox(height: 16);
   static const SizedBox h24 = SizedBox(height: 24);
-  static const SizedBox h60 = SizedBox(height: 90);
+  static const SizedBox h70 = SizedBox(height: 60);
 
   // Horizontal spaces
   static const SizedBox w8 = SizedBox(width: 8);
